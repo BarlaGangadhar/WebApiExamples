b0fcaa95af0e7a4848296481a807efd1f3d1e362
BarlaGangadhar <gangadharbarla057@gmail.com>
2022-10-15 17:31:21 +0530

    add id

--- WebApiExamples/Models/Users.cs
+++ WebApiExamples/Models/Users.cs
@@ -7,7 +7,7 @@ namespace WebApiExamples.Models
 {
     public class Users
     {
-
+        public int userid { get; set; }
         public int ID { get; set; }
         public string UserName { get; set; }
         public string Password { get; set; }
